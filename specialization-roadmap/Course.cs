6ca5850a79089f3bb1b138696de5db4cd0d486cb
ron-ligsay <akyligsay16@gmail.com>
2023-07-17 20:09:24 +0800

    create course interface

---
+++ specialization-roadmap/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace specialization_roadmap
+{
+    public interface ICourse
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public float Progress { get; set; }
+        public bool status { get; set; }
+
+    }
+}
