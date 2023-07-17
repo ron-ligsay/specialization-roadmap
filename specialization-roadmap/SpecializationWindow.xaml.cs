40f446b0ee2b8cafa6bdf0af000b648a599a4c41
ron-ligsay <akyligsay16@gmail.com>
2023-07-17 21:56:59 +0800

    specialization roadmap windows

---
+++ specialization-roadmap/SpecializationWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using specialization_roadmap.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace specialization_roadmap
+{
+    /// <summary>
+    /// Interaction logic for SpecializationWindow.xaml
+    /// </summary>
+    public partial class SpecializationWindow : Window
+    {
+        public SpecializationWindow()
+        {
+            InitializeComponent();
+
+            List<specialization> track = new List<specialization>();
+
+            specialization special_frontend = new specialization();
+            special_frontend.Id = 1000;
+            special_frontend.Title = "Front-End Developer";
+            special_frontend.Description = "Designs and creates the look of a website.";
+            special_frontend.Progress = 0.0;
+            special_frontend.status = false;
+
+
+            track.Add(special_frontend);
+        }
+    }
+}
