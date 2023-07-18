595b9a910ad6ba7880b67c8f315148f0579fd577
ron-ligsay <akyligsay16@gmail.com>
2023-07-18 14:00:24 +0800

    created roadmap steps window

---
+++ specialization-roadmap/RoadmapStepsWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace specialization_roadmap
+{
+    /// <summary>
+    /// Interaction logic for RoadmapStepsWindow.xaml
+    /// </summary>
+    public partial class RoadmapStepsWindow : Window
+    {
+        public RoadmapStepsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
