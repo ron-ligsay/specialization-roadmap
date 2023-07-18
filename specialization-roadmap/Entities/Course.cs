25e28fbea6adc5d1a730e458ffad75daa1f66d15
ron-ligsay <akyligsay16@gmail.com>
2023-07-18 20:19:11 +0800

    remove comment

--- specialization-roadmap/Entities/Course.cs
+++ specialization-roadmap/Entities/Course.cs
@@ -11,7 +11,6 @@ namespace specialization_roadmap.Entities
         public static int Id { get; set; }
         public static string Title { get; set; } = string.Empty;
         public static string Description { get; set; } = string.Empty;
-        //public static double Progress { get; set; }
         public static bool Status { get; set; }
 
     }
