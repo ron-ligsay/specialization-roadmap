e266501297f1d55989affc894afcbb273b86455e
ron-ligsay <akyligsay16@gmail.com>
2023-07-19 22:12:06 +0800

    created a model viewing

---
+++ specialization-roadmap/Model/ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using specialization_roadmap.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace specialization_roadmap.Model
+{
+    internal class ViewModel
+    {
+        public IEnumerable<SpecializationModel>? specializationModels;
+
+       // public IEnumerable<SpecializationModel> SpecializationModels { get => specializationModels; set => specializationModels = value; }
+    }
+}
