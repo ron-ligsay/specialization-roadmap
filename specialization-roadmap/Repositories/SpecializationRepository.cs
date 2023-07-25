78438222e4a4a48e4d161769ff83b43ec55e3419
ron-ligsay <akyligsay16@gmail.com>
2023-07-25 17:26:41 +0800

    created more methods and lisst

--- specialization-roadmap/Repositories/SpecializationRepository.cs
+++ specialization-roadmap/Repositories/SpecializationRepository.cs
@@ -1,6 +1,7 @@
 ﻿using specialization_roadmap.Entities;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,18 @@ namespace specialization_roadmap.Repositories
     /// </summary>
     public class SpecializationRepository : SpecializationModel
     {
+
+        public SpecializationModel specializationModel { get; set; }
+
+        public SpecializationModel SelectedModel { get; set; }
+
+        public List<SpecializationModel> specializationModels { get; set; }
+
+        public ObservableCollection<SpecializationModel> GetAllSpecializationTrackObservation()
+        {
+            return DataSourceObservation();
+        }
+
         public List<SpecializationModel> GetAllSpecializationTrack() 
         {
             return DataSource();
@@ -37,7 +50,13 @@ namespace specialization_roadmap.Repositories
         {
             return DataSource().Where(x=> x.Title.Contains(name) && x.Status == status).ToList();
         }
-        
+
+        /*
+        internal IEnumerable<SpecializationModel> GetAllSpecialization()
+        {
+            throw new NotImplementedException();
+        }*/
+
         // Creating Specialization Repository
         private List<SpecializationModel> DataSource() 
         {
@@ -48,18 +67,49 @@ namespace specialization_roadmap.Repositories
                     Progress=1,Status=true,Rating=3.5
                 },
                 new SpecializationModel() {
-                    Id=1, Title="Back-end Developer", Description="Back-end Developer Description",
+                    Id=2, Title="Back-end Developer", Description="Back-end Developer Description",
                     Progress=1,Status=true,Rating=4.5
                 },
                 new SpecializationModel() {
-                    Id=1, Title="Full Stack Developer", Description="Full Stack Developer Description",
+                    Id=3, Title="Full Stack Developer", Description="Full Stack Developer Description",
                     Progress=0.0,Status=false,Rating=1.5
                 },
                 new SpecializationModel() {
-                    Id=1, Title="Application Developer", Description="Application Developer Description",
+                    Id=4, Title="Application Developer", Description="Application Developer Description",
                     Progress=0.0,Status=false,Rating=5.0
                 }
             };
         }
+
+        // Creating Specialization Repository
+        private ObservableCollection<SpecializationModel> DataSourceObservation()
+        {
+            return new ObservableCollection<SpecializationModel>()
+            {
+                new SpecializationModel() {
+                    Id=1, Title="Font-end Developer", Description="Font-end Developer Description",
+                    Progress=1,Status=true,Rating=3.5
+                },
+                new SpecializationModel() {
+                    Id=2, Title="Back-end Developer", Description="Back-end Developer Description",
+                    Progress=1,Status=true,Rating=4.5
+                },
+                new SpecializationModel() {
+                    Id=3, Title="Full Stack Developer", Description="Full Stack Developer Description",
+                    Progress=0.0,Status=false,Rating=1.5
+                },
+                new SpecializationModel() {
+                    Id=4, Title="Application Developer", Description="Application Developer Description",
+                    Progress=0.0,Status=false,Rating=5.0
+                }
+            };
+        }
+
+        public SpecializationRepository()
+        {
+            this.specializationModels = this.DataSource();
+
+        }
+
     }
 }
