12a435df09b27f13675bb4d0cf56e6bb51d72343
ron-ligsay <akyligsay16@gmail.com>
2023-07-26 11:24:12 +0800

    remove interface and direct to model

--- specialization-roadmap/Entities/roadmap.cs
+++ specialization-roadmap/Entities/roadmap.cs
@@ -6,98 +6,15 @@ using System.Threading.Tasks;
 
 namespace specialization_roadmap.Entities
 {
-    public interface IRoadmap : ISpecialization
-    {
-        public new int Id { get; set; }
-        public new string Title { get; set; }
-        public new string Description { get; set; }
-        public new bool Status { get; set; }
-        
-        public new int Rating { get; set; }
-        public List<int>? specializationStepAt { get; set; }
-        public List<int>? specializationStepOrderAt { get; set; }
-        public List<string> ResourcesLinks { get; set; }
-    }
 
-    public class RoadmapStepModel : IRoadmap
+    public class RoadmapStepModel : ICourse
     {
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public int Step { get; set; }
         public bool Status { get; set; }
-
         public int Rating { get; set; }
-
-        /// <summary>
-        /// Reference ID List to ID of the Specialization
-        /// </summary>
-        public List<int> specializationStepAt { get; set; }
-        
-        /// <summary>
-        /// Declares the step order in the roadmap
-        /// </summary>
-        public List<int> specializationStepOrderAt { get; set; }
         public List<string> ResourcesLinks { get; set; }
-
-        
-
-
-        public bool containsSpecializationId(int specializationId)
-        {
-            return specializationStepAt.Contains(specializationId);
-        }
-
-        public bool containsNextStepId(int stepIndex, int nextStep)
-        {
-            return specializationStepOrderAt[stepIndex] == nextStep;
-        }
-        
-        public int getStepNumberofRoadmap(int step)
-        {
-            return specializationStepOrderAt[step];
-        }
-
-        int index;
-        public int getIndexofSpecializationId(List<int> specializationIdList, int specilizationId)
-        {
-            //return specializationStepAt.IndexOf(specializationIdList, specilizationId);
-            
-            for (int i=0; i<specializationIdList.Count; i++)
-            {
-                int temp = specializationIdList[i];
-                index = specializationIdList.IndexOf(temp);
-            }
-            return index;
-        }
-
-
-
-        public List<RoadmapStepModel> CreateSpecializationModelList()
-        {
-            List<RoadmapStepModel> roadmapStepModels = new List<RoadmapStepModel>();
-
-            RoadmapStepModel internetRoadmapStepModel = new RoadmapStepModel();
-            internetRoadmapStepModel.Id = 1;
-            internetRoadmapStepModel.Title = "Internet";
-            internetRoadmapStepModel.Description = "The Internet is a global network of computers connected to each other which communicate through a standardized set of protocols.";
-            internetRoadmapStepModel.Status = true;
-            internetRoadmapStepModel.Rating = 3;
-            List<int> stepAt = new List<int>() { 1 };
-            internetRoadmapStepModel.specializationStepAt = stepAt;
-            List<int> orderAt = new List<int>() { 1 };
-            internetRoadmapStepModel.specializationStepOrderAt = orderAt;
-            List<string> links = new List<string>() {
-                "https://cs.fyi/guide/how-does-internet-work",
-                "https://www.vox.com/2014/6/16/18076282/the-internet",
-                "http://web.stanford.edu/class/msande91si/www-spr04/readings/week1/InternetWhitepaper.htm",
-                "https://roadmap.sh/guides/what-is-internet"
-            };
-            internetRoadmapStepModel.ResourcesLinks = links;
-
-            roadmapStepModels.Add(internetRoadmapStepModel);
-
-            return roadmapStepModels;
-
-        }
     }
 }
